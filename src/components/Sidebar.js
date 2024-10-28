53a36f2b8c9807a331da48d641541b6f5cf41dd7
AndresSu2342 <cesar.borray-s@mail.escuelaing.edu.co>
2024-10-28 01:19:36 -0500

    Adaptacion Boton Usuario

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,16 +1,14 @@
-// Sidebar.js
 import React from 'react';
 import '../styles/sidebar.css'; // Importar los estilos específicos del Sidebar
 
 const role = localStorage.getItem('role');
-const Sidebar = ({ onNavigate }) => {
 const Sidebar = ({ onNavigate, openUserModal }) => {
     return (
         <div className="sidebar">
             <button onClick={() => onNavigate('taskManager')}>
                 <i className="fa-solid fa-list-check"></i>
             </button>
-            {role === "2" && (
+            {role === "1" && (
                 <button onClick={() => onNavigate('insights')}>
                     <i className="fa-solid fa-chart-column"></i>
                 </button>
